d6edc9e1dcc21d3d7904fed67b72c5f8b52977ba
lazaralex98 <alex2012ro@gmail.com>
2022-02-03 07:22:48 +0200

    preparing deployment

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,29 +1,19 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  /**
+   * this is the address of my Mumbai TCO2 (Toucan Protocol: TCO2-VCS-439-2008)
+   * from Yingpeng HFC23 Decompostion Project
+   */
+  const tco2Address = "0xa5831eb637dff307395b5183c86B04c69C518681";
+  const FaucetFactory = await ethers.getContractFactory("TCO2Faucet");
+  const faucet = await FaucetFactory.deploy(tco2Address);
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  await faucet.deployed();
 
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("TCO2 Faucet deployed to: ", faucet.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
